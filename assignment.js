b0510f1463188d5d6ef081e27b7438e874d96f9c
Lahaduzzaman Lahad <criclahad.2@gmail.com>
2021-01-22 15:08:02 +0600

    Kilometer TO Meter

---
+++ assignment.js
@@ -0,0 +1,10 @@
+// Kilometer TO Meter
+
+function kilometerToMeter(kiloMeter) {
+    if (kiloMeter < 0) {
+      return "ERORR!!! there is no negative value in kilometer!";
+    } else {
+      var meter = kiloMeter * 1000;
+      return meter;
+    }
+  }
